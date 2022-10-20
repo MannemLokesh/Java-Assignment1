06c111d81689662b1b91a5e98e4aadff72df8088
MannemLokesh <110220365+MannemLokesh@users.noreply.github.com>
2022-10-20 16:45:45 +0530

    Update Program3.java

--- Program3.java
+++ Program3.java
@@ -2,12 +2,12 @@ public class Program3
 {
 	public static void main(String[] args)
 	{
-		int n=14;
-		for(int i=0;i<n;i++)
+		int num=14;
+		for(int i=0;i<num;i++)
 		{
-			for(int j=0;j<n; j++)
+			for(int j=0;j<num; j++)
 			{
-				if(i==0||i==n-1||j==0 || j==n-1 || i+j<=(n-1)/2 || j-i>=(n-1)/2  
+				if(i==0||i==num-1||j==0 || j==num-1 || i+j<=(num-1)/2 || j-i>=(num-1)/2  
 						 )
 				{
 					System.out.print("* ");
