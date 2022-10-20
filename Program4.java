26a3c7f2b2bb76a03b6e81b449a6c4a79a2b6eb3
MannemLokesh <110220365+MannemLokesh@users.noreply.github.com>
2022-10-20 16:46:35 +0530

    Update Program4.java

--- Program4.java
+++ Program4.java
@@ -2,13 +2,13 @@ public class Program4
 {
 	public static void main(String[] args)
 	{
-		int n=14;
-		for(int i=0;i<n;i++)
+		int num=14;
+		for(int i=0;i<num;i++)
 		{
-			for(int j=0;j<n; j++)
+			for(int j=0;j<num; j++)
 			{
-				if((j==0) && (i>=(n-1)/2)|| i==n-1 || (j==(n-1)) && (i>=(n-1)/2) ||
-					(i-j>=(n-1)/2) || (i+j>=(n+(n-3)/2))
+				if((j==0) && (i>=(num-1)/2)|| i==num-1 || (j==(num-1)) && (i>=(num-1)/2) ||
+					(i-j>=(num-1)/2) || (i+j>=(num+(num-3)/2))
 						 )
 				{
 					System.out.print("* ");
