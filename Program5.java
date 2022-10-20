9987fe2e3456fa1a2bbead81aace7b55d2652493
MannemLokesh <110220365+MannemLokesh@users.noreply.github.com>
2022-10-20 16:47:58 +0530

    Update Program5.java

--- Program5.java
+++ Program5.java
@@ -2,13 +2,12 @@ public class Program5
 {
 	public static void main(String[] args)
 	{
-		int n=14;
-		for(int i=0;i<n;i++)
+		int num=14;
+		for(int i=0;i<num;i++)
 		{
-			for(int j=0;j<n; j++)
+			for(int j=0;j<num; j++)
 			{
-				if(i==0 || j==0 || i==(n-1) || (i+j<=((n-1)/2)) || (i-j>=((n-1)/2))
-						 )
+				if(i==0 || j==0 || i==(num-1) || (i+j<=((num-1)/2)) || (i-j>=((num-1)/2)))
 				{
 					System.out.print("* ");
 				}
